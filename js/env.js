dc8a02b36908c3bfbdd27b1c0d1c896cdaaa2dc5
nadunys <yashnadun@gmail.com>
2022-08-24 20:48:25 +0530

    sun and moon

--- js/env.js
+++ js/env.js
@@ -1,4 +1,3 @@
-// stars
 class Star {
   constructor(options) {
     this.size = Math.random() * 2;
@@ -7,20 +6,75 @@ class Star {
     this.y = options.y;
   }
 
-  reset(width, height) {
+  reset(w, h) {
     this.size = Math.random() * 2;
     this.speed = Math.random() * 0.05;
-    this.x = width;
-    this.y = Math.random() * height;
+    this.x = w;
+    this.y = Math.random() * h;
   }
 
-  update(bgCtx, width, height) {
+  update(ctx, w, h) {
     this.x -= this.speed;
     if (this.x < 0) {
-      this.reset(width);
+      this.reset(w, h);
     } else {
-      bgCtx.fillRect(this.x, this.y, this.size, this.size);
+      ctx.fillRect(this.x, this.y, this.size, this.size);
     }
+  }
+}
+
+class Sun {
+  constructor(options) {
+    this.r = options.r;
+    this.x = options.x;
+    this.y = options.y;
+  }
+
+  reset(w, h) {}
+
+  update(ctx, w, h) {
+    ctx.beginPath();
+    ctx.lineWidth = 8;
+    ctx.strokeStyle = "#f5eb71";
+    ctx.fillStyle = "#F6D602";
+
+    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.fill();
+  }
+}
+
+class Moon {
+  constructor(options) {
+    this.r = options.r;
+    this.x = options.x;
+    this.y = options.y;
+  }
+
+  reset(w, h) {}
+
+  update(ctx, w, h) {
+    ctx.lineWidth = 4;
+    ctx.strokeStyle = "#f5eb71";
+    ctx.fillStyle = "#d3d3e3";
+
+    ctx.beginPath();
+    ctx.arc(
+      this.x,
+      this.y,
+      this.r,
+      (Math.PI / 180) * 40,
+      (Math.PI / 180) * 320
+    );
+    ctx.bezierCurveTo(
+      this.x - 40,
+      this.y - 50,
+      this.x - 40,
+      this.y + 50,
+      this.x + 50,
+      this.y + 50
+    );
+    ctx.fill();
   }
 }
 
@@ -32,23 +86,25 @@ function createNightSky(background, bgCtx) {
 
   background.width = width;
   background.height = height;
-  bgCtx.fillStyle = "#05004c";
 
   var entities = [];
 
+  // init the sun
+  entities.push(new Sun({ x: width / 2, y: height / 4, r: 75 }));
+
   // init the stars
-  for (var i = 0; i < height; i++) {
-    entities.push(
-      new Star({
-        x: Math.random() * width,
-        y: Math.random() * height,
-      })
-    );
-  }
+//   for (var i = 0; i < height; i++) {
+//     entities.push(
+//       new Star({
+//         x: Math.random() * width,
+//         y: Math.random() * height,
+//       })
+//     );
+//   }
 
   //animate background
   function animate() {
-    bgCtx.fillStyle = "#110E19";
+    bgCtx.fillStyle = "white";
     bgCtx.fillRect(0, 0, width, height);
     bgCtx.fillStyle = "#ffffff";
     bgCtx.strokeStyle = "#ffffff";
